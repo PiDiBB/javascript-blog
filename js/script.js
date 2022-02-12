21acc419fc0f8e09284a0c88d965693046b9e1bf
Przemyslaw Damek <pdamek.bb@gmail.com>
2022-02-12 12:05:29 +0100

    Errors - need Mentor's directions

--- js/script.js
+++ js/script.js
@@ -24,10 +24,54 @@ function titleClickHandler(event){
   activeArticle.classList.add('active');
 }
 
-
-
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  console.log (titleList);
+  /* NIE WIEM JAK ZASTOSOWAĆ clearMessages */
+
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+  console.log(articles);
+
+  for(let article of articles){} /* PĘTLA DLA WSZYSTKICH ARTYKUŁÓW - PO CO W TYM MOMENCIE??? */
+    
+  /* get the article id */
+
+  const articleId = clickedElement.getAttribute('Id');
+  /* script.js:55 Uncaught ReferenceError: clickedElement is not defined
+    at generateTitleLinks (script.js:55:21)
+    at script.js:76:1 - CZY clidkedElement zdefiniowany powyżej nie powinien odnosić się do tego miejsca również?? */ 
+
+  /* find the title element */
+
+  const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+  /* get the title from the title element */
+
+  const title = clickedElement.getAttribute(articleTitle)
+
+  /* create HTML of the link */
+
+  const linkHTML = '<li><a href="# + articleId + "><span>' + articleId + '</span></a></li>';
+  console.log(linkHTML)
+
+  /* insert link into titleList */
+
+  titleList.innerHTML = titleList.innerHTML + linkHTML;
+
+}
+
+
+generateTitleLinks();
