376cc32f3c02a226a9c0ff97e72222e6d50641e6
Przemyslaw Damek <pdamek.bb@gmail.com>
2022-02-12 09:04:02 +0100

    Finish titleClickHandler function

--- js/script.js
+++ js/script.js
@@ -1,29 +1,30 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();
   const clickedElement = this;
-  console.log('Link was clicked!');
-
-  /* [DONE] remove class 'active' from all article links  */
-
+  
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-
-  /* [IN PROGRESS] add class 'active' to the clicked link */
   clickedElement.classList.add('active');
-  console.log('clickedElement:', clickedElement);
 
-  /* [DONE] remove class 'active' from all articles */
+  const articleList = document.querySelectorAll('.posts .post');
 
-  /* get 'href' attribute from the clicked link */
+  for (let article of articleList) {
+    article.classList.remove('active');
+  }
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  const articleSelector = clickedElement.getAttribute('href');
 
-  /* add class 'active' to the correct article */
+  const activeArticle = document.querySelector(articleSelector);
+
+  activeArticle.classList.add('active');
 }
+
+
 
 const links = document.querySelectorAll('.titles a');
 
